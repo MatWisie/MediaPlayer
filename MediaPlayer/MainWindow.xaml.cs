9d2496c238473fbb492d81631e410d5d7c77c35c
MatWisie <Mateuszwisiecki@gmail.com>
2021-09-23 20:11:39 +0200

    Added the open file function. From now the user can choose the source of the mediaelement.

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -164,6 +165,21 @@ namespace MediaPlayer
                     TitleBar.Visibility = Visibility.Visible;
                 }
             }
+        }
+
+        private void FileDialog(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog a = new OpenFileDialog();
+            a.Filter = "Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV"; ;
+            a.ShowDialog();
+
+            if (!string.IsNullOrEmpty(a.FileName))
+            {
+                Media.Source = new Uri(a.FileName);
+            }
+
+
         }
     }
 }
