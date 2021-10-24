fbf65a583431bfc422d55725169057f04edb952e
MatWisie <Mateuszwisiecki@gmail.com>
2021-10-24 19:39:38 +0200

    Added main menu window

---
+++ MediaPlayer/MainMenu.xaml.cs
@@ -0,0 +1,63 @@
+﻿using MediaPlayer.Properties;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _windowMenu.DragMove();
+        }
+
+        private void Button_Exit(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Button_WindowMaximized(object sender, RoutedEventArgs e)
+        {
+            if (_windowMenu.WindowState != WindowState.Maximized)
+                _windowMenu.WindowState = WindowState.Maximized;
+            else
+                _windowMenu.WindowState = WindowState.Normal;
+        }
+
+        private void Button_WindowMinimized(object sender, RoutedEventArgs e)
+        {
+            _windowMenu.WindowState = WindowState.Minimized;
+        }
+
+        private void FileDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog MediaSource = new OpenFileDialog();
+            MediaSource.Filter = "Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV"; ;
+            MediaSource.ShowDialog();
+            MainWindow mainwindow = new MainWindow();
+            mainwindow.ShowDialog();
+
+            
+        }
+    }
+}
